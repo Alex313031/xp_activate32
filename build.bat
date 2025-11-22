390819687f92653fbb0b354fcb4f81ce57633e59
Alexander Frick <Alex313031@gmail.com>
2025-11-21 22:17:04 -0600

    rename files, update readme and make copy of old readme, dont run program after building it in build.bat

---
+++ build.bat
@@ -0,0 +1,1 @@
+rc.exe xp_activate32.rc && cl.exe /arch:IA32 /O2 /GS- xp_activate32.c xp_activate32.res /link /fixed /entry:entry /subsystem:windows,5.1 kernel32.lib user32.lib msvcrt.lib ole32.lib oleaut32.lib comctl32.libs
