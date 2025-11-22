e6a78edb26f408e9022c991b2459ec88eca5be00
Alexander Frick <Alex313031@gmail.com>
2025-11-22 00:17:30 -0800

    add version.h, constants.h (and move constants there), and proper VERSIONINFO in .rc file

--- src/resource.h
+++ src/resource.h
@@ -3,7 +3,11 @@
 // Used by Resource.rc
 //
 
+#pragma once
+
 #pragma code_page(65001) // UTF-8
+
+#include "version.h"
 
 #define IDI_MAINFRAME                   103
 #define IDD_MAINFRAME                   100
@@ -13,12 +17,17 @@
 #endif // IDC_STATIC
 
 // Next default values for new objects
-// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
-#define _APS_NEXT_SYMED_VALUE           101
-#endif
-#endif
+ // See https://learn.microsoft.com/en-us/cpp/mfc/tn035-using-multiple-resource-files-and-header-files-with-visual-cpp
+ #define _APS_NO_MFC                 130
+ #define _APS_NEXT_RESOURCE_VALUE    103
+ #define _APS_NEXT_COMMAND_VALUE     40001
+ #define _APS_NEXT_CONTROL_VALUE     1001
+ #define _APS_NEXT_SYMED_VALUE       101
+ // New MSVS projects use these.
+ //#define _APS_NEXT_COMMAND_VALUE     32771
+ //#define _APS_NEXT_CONTROL_VALUE     1000
+ //#define _APS_NEXT_SYMED_VALUE       103
+#endif // APSTUDIO_READONLY_SYMBOLS
+#endif // APSTUDIO_INVOKED
---
+++ src/version.h
@@ -0,0 +1,34 @@
+#ifndef XP_ACTIVATE32_VERSION_H_
+#define XP_ACTIVATE32_VERSION_H_
+
+#pragma code_page(65001) // UTF-8
+
+// Macro to convert to string
+#if !defined(_STRINGIZER) && !defined(STRINGIZE)
+ #define _STRINGIZER(in) #in
+ #define STRINGIZE(in) _STRINGIZER(in)
+#endif
+
+// Main version constant
+#ifndef _VERSION
+ // Run stringizer above
+ #define _VERSION(major,minor,build) STRINGIZE(major) "." STRINGIZE(minor) "." STRINGIZE(build)
+#endif
+
+// These next few lines are where we control version number and copyright year
+// Adhere to semver > semver.org
+#define MAJOR_VERSION 1
+#define MINOR_VERSION 0
+#define BUILD_VERSION 1
+
+#define MAIN_TITLE L"XP_Activate32"
+
+#ifndef VERSION_STRING
+ #define VERSION_STRING _VERSION(MAJOR_VERSION, MINOR_VERSION, BUILD_VERSION)
+ #define ABOUT_TITLE L"About XP_Activate32"
+ #define ABOUT_VERSION L"xp_activate32 ver. 1.0.1"
+ #define ABOUT_COPYRIGHT L"Copyright © 2025 Alex313031"
+ #define LEGAL_COPYRIGHT L"© 2025"
+#endif // VERSION_STRING
+
+#endif // XP_ACTIVATE32_VERSION_H_
