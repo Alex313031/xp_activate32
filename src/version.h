f787a7a6a780289a9ddba2a02bad17165584864c
Alexander Frick <Alex313031@gmail.com>
2025-11-22 02:50:59 -0800

    set target win32 version info in version.h

--- src/version.h
+++ src/version.h
@@ -3,6 +3,19 @@
 
 #pragma code_page(65001) // UTF-8
 
+#include <WinSDKVer.h>
+
+#define WINVER _WIN32_WINNT_WINXP
+#define _WIN32_WINNT _WIN32_WINNT_WINXP
+#define _WIN64_WINNT _WIN32_WINNT_WS03s // Minimum version for 64 bit
+#define _WIN32_IE 0x0600 // Minimum Internet Explorer version
+
+// See https://tedwvc.wordpress.com/2014/01/01/how-to-target-xp-with-vc2012-or-vc2013-and-continue-to-use-the-windows-8-x-sdk/
+//#define _USING_V110_SDK71_
+#define _ATL_XP_TARGETING
+
+#include <SDKDDKVer.h>
+
 // Macro to convert to string
 #if !defined(_STRINGIZER) && !defined(STRINGIZE)
  #define _STRINGIZER(in) #in
