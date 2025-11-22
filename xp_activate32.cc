f0a8be54858bf7a5fad615fdba11e35315d6376b
Alexander Frick <Alex313031@gmail.com>
2025-11-21 22:52:41 -0600

    move some things to xp_activate32.h header file

--- xp_activate32.cc
+++ xp_activate32.cc
@@ -1,20 +1,9 @@
-#define STRICT
-#define UNICODE
-#define _UNICODE
+#include "xp_activate32.h"
 
 #include <windows.h>
 #include <commctrl.h>
-#include <stdint.h>
-#include <intrin.h>
 
 #define assert(x) /*nothing*/
-
-typedef int64_t i64;
-typedef uint64_t ui64;
-
-#define MOD 0x16A6B036D7F2A79ULL
-#define NON_RESIDUE 43
-static const ui64 f[6] = {0, 0x21840136C85381ULL, 0x44197B83892AD0ULL, 0x1400606322B3B04ULL, 0x1400606322B3B04ULL, 1};
 
 typedef struct {
 	ui64 u[2];
--- xp_activate32.h
+++ xp_activate32.h
@@ -1,4 +1,34 @@
 #ifndef XP_ACTIVATE32_H_
 #define XP_ACTIVATE32_H_
 
+#include <stdint.h>
+#include <intrin.h>
+
+#ifndef MAJOR_VERSION
+ #define MAJOR_VERSION 1
+ #define MINOR_VERSION 0
+ #define PATCH_VERSION 1
+#endif
+
+#ifndef STRICT
+ #define STRICT
+#endif 
+
+#ifndef UNICODE
+ #define UNICODE
+#endif
+
+#ifndef _UNICODE
+ #define _UNICODE
+#endif
+
+typedef int64_t i64;
+typedef uint64_t ui64;
+
+#define MOD 0x16A6B036D7F2A79ULL
+
+#define NON_RESIDUE 43
+
+static const ui64 f[6] = {0, 0x21840136C85381ULL, 0x44197B83892AD0ULL, 0x1400606322B3B04ULL, 0x1400606322B3B04ULL, 1};
+
 #endif // XP_ACTIVATE32_H_
