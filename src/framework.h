c08fdccc062253c233a9a19fbcc68d325ddef428
Alexander Frick <Alex313031@gmail.com>
2025-11-23 19:02:00 -0800

    add BUILD.gn and corrections for compiling with Clang under GN/Ninja

--- src/framework.h
+++ src/framework.h
@@ -2,14 +2,24 @@
 
 #include <WinSDKVer.h>
 
-#define WINVER _WIN32_WINNT_WINXP
-#define _WIN32_WINNT _WIN32_WINNT_WINXP
-#define _WIN64_WINNT _WIN32_WINNT_WS03s // Minimum version for 64 bit
-#define _WIN32_IE 0x0600 // Minimum Internet Explorer version
+#ifndef WINVER
+ #define WINVER _WIN32_WINNT_WINXP
+#endif // WINVER
+#ifndef _WIN32_WINNT
+ #define _WIN32_WINNT _WIN32_WINNT_WINXP
+#endif // _WIN32_WINNT
+#ifndef _WIN64_WINNT
+ #define _WIN64_WINNT _WIN32_WINNT_WS03s // Minimum version for 64 bit
+#endif // _WIN64_WINNT
+#ifndef _WIN32_IE
+ #define _WIN32_IE 0x0600 // Minimum Internet Explorer version
+#endif // _WIN32_IE
 
 // See https://tedwvc.wordpress.com/2014/01/01/how-to-target-xp-with-vc2012-or-vc2013-and-continue-to-use-the-windows-8-x-sdk/
 //#define _USING_V110_SDK71_
-#define _ATL_XP_TARGETING
+#ifndef _ATL_XP_TARGETING
+ #define _ATL_XP_TARGETING
+#endif // _ATL_XP_TARGETING
 
 #include <SDKDDKVer.h>
 
